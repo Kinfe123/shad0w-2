ff8e50f22815550226b865e181bac6721e51593d
Kinfe123 <kinfishtech@gmail.com>
2024-01-22 14:48:39 +0300

    wip

--- components/happy-rectangle.tsx
+++ components/happy-rectangle.tsx
@@ -1,31 +1,32 @@
-'use client'
+"use client"
 
-import { stylesWithCssVar } from "@/lib/motion";
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
+
+import { stylesWithCssVar } from "@/lib/motion"
 
 export const HappyRect = () => {
-  const targetRef = useRef<HTMLDivElement | null>(null);
+  const targetRef = useRef<HTMLDivElement | null>(null)
   const { scrollYProgress } = useScroll({
     target: targetRef,
     offset: ["start end", "end end"],
-  });
-  const textX = useTransform(scrollYProgress, [0.1, 0.7], ["100%", "-100%"]);
-  const opacitySection = useTransform(scrollYProgress, [0.1, 0.5], [0, 1]);
-  const scale = useTransform(scrollYProgress, [0.1, 0.7], [1, 0.7]);
+  })
+  const textX = useTransform(scrollYProgress, [0.1, 0.7], ["80%", "-100%"])
+  const opacitySection = useTransform(scrollYProgress, [0.1, 0.5], [0, 1])
+  const scale = useTransform(scrollYProgress, [0.1, 0.7], [1, 0.7])
 
   const opacityBorder = useTransform(
     scrollYProgress,
     [0.7, 0.71, 0.72],
     [1, 1, 0]
-  );
+  )
   const finalTextOpacity = useTransform(
     scrollYProgress,
     [0.7, 0.71, 0.72, 0.8, 0.9],
     [0, 0, 1, 1, 0]
-  );
+  )
 
-  const finalTextScale = useTransform(scrollYProgress, [0.8, 0.9], [1, 0.7]);
+  const finalTextScale = useTransform(scrollYProgress, [0.8, 0.9], [1, 0.7])
 
   return (
     <motion.section
@@ -62,22 +63,18 @@ export const HappyRect = () => {
           }}
           className="absolute  left-1/2 top-1/2 space-y-[-100px]  bg-gradient-to-tr from-[#E0B379] to-[#e8eaed] bg-clip-text text-transparent  font-heading2 text-center  leading-[1] text-dark text-[20rem]"
         >
-          PROUD 
-          
+          PROUD
           <br />
-          <span className="">
-          PRODUCT
-
-          </span>
+          <span className="">PRODUCT</span>
         </motion.p>
 
         <canvas
-            className="bg-skin-base pointer-events-none absolute inset-0"
-            id="canvas"
-          ></canvas>
+          className="bg-skin-base pointer-events-none absolute inset-0"
+          id="canvas"
+        ></canvas>
         <span className="absolute left-[calc(50%*var(--scale)+50%)] top-0 z-10 h-full w-[50vw] origin-left scale-[var(--scale)] bg-transparent opacity-[var(--opacity-border)]" />
         <span className="absolute left-[calc(50%*var(--scale)+50%-(2.5rem*var(--scale)))] top-0 z-[12] h-full w-[50vw] origin-left scale-[var(--scale)] border-l-[2.5rem] border-[#f1c944] opacity-[var(--opacity-border)]" />
       </div>
     </motion.section>
-  );
-};
+  )
+}
