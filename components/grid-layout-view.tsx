de026f94230ec81ff0626bf3a1ad7cbcf544bcc6
Kinfe123 <kinfishtech@gmail.com>
2024-01-27 00:14:00 +0300

    prod test

--- components/grid-layout-view.tsx
+++ components/grid-layout-view.tsx
@@ -75,20 +75,20 @@ const cards = [
     content: "Luncar Eclipse",
     className: "col-span-1",
     thumbnail:
-      "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/startup3.jpg",
   },
   {
     id: 3,
     content:'Astronomical Event',
     className: "col-span-1",
     thumbnail:
-      "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/startup2.jpg",
   },
   {
     id: 4,
     content: 'Pretty Little Fears',
     className: "md:col-span-2",
     thumbnail:
-      "https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "/startup1.jpg",
   },
 ];
