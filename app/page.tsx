9adc1c49087ec8746cd72bb82c07d25631255584
Kinfe123 <kinfishtech@gmail.com>
2024-01-27 00:06:20 +0300

    fix: ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,6 @@ export default function IndexPage() {
             </div>
             <ShiftingCountdown />
           </div>
-
           <div className=" grid grid-cols-2 gap-1">
             <div className="mt-[-500x] flex  justify-center items-center bg-transparent px-10">
               <p className="d:text-4xl mt-[-700px] font-heading2 lg:text-6xl xl:text-[12rem] font-medium bg-gradient-to-tr from-[#E0B379] to-[#e8eaed] bg-clip-text text-transparent">
@@ -55,16 +54,13 @@ export default function IndexPage() {
           <div>
             <LayoutGridDemo />
           </div>
-          {/* <div>
-            <HoveryCard />
-          </div> */}
+      
           <div>
             <StartUpDemo />
           </div>
         </section>
       </ScrollProvider>
-      {/* to be fixed */}
-      {/* <BlendThought /> */}
+     
     </div>
   )
 }
