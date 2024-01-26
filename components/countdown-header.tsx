d2a781d9985c6082f4099b93f8f6763ccf6148f0
Kinfe123 <kinfishtech@gmail.com>
2024-01-27 00:09:08 +0300

    fix: ui fix

--- components/countdown-header.tsx
+++ components/countdown-header.tsx
@@ -9,8 +9,6 @@ const CountDownTimerHeader = () => {
     target: divRef,
   })
   const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0.4, 0])
-  console.log("the opacity", opacity)
-
   return (
     <motion.div ref={divRef} style={{ opacity }} className="flex justify-center items-center font-heading2 my-10">
       {/* <h1 className="dark:bg-gradient-to-tr dark:from-[#E0B379] dark:to-[#f0cb8a] dark:bg-clip-text dark:text-transparent text-5xl md:text-7xl text-center ">We are almost there</h1> */}
